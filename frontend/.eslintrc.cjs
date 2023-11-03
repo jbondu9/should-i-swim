55642ffc8c0208ea62bf7a56ef87b2294385ef7b
Jimmy Bondu <jimmy.bondu@zoho.com>
2023-11-03 15:35:39 +0100

    build: :heavy_plus_sign: add eslint

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ["plugin:vue/vue3-recommended", "prettier"],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  plugins: ["vue"],
+  rules: {
+    "vue/no-unused-vars": "error",
+  },
+};
