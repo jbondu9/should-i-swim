4b1ea2cd8e2b69525a3ae256bc48689d02b53c4c
Jimmy Bondu <jimmy.bondu@zoho.com>
2023-11-04 12:57:39 +0100

    test: :white_check_mark: add tests on answers

--- backend/answers/tests.py
+++ backend/answers/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.urls import reverse_lazy
 
-# Create your tests here.
+from rest_framework.test import APITestCase
+
+from answers.enums import Reason
+
+
+class TestAnswer(APITestCase):
+    url = reverse_lazy("answer-list")
+
+    def test_list_without_params(self):
+        response = self.client.get(self.url)
+
+        data = response.json()
+
+        expected_fields = ["reason_name", "reason_description"]
+
+        self.assertEqual(response.status_code, 200)
+        self.assertListEqual(list(data), expected_fields)
+
+    def test_list_with_params(self):
+        response = self.client.get(self.url, data={"open": "true", "bound": "72.1"})
+
+        data = response.json()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data["reason_name"], Reason.CROWED.value)
